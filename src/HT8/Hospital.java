cb028a1f8c06016858140f12d8ebdb6f6b82099f
estebancabreraa <esteban.cabrera.a@gmail.com>
2018-04-04 19:31:19 -0600

    Clase Hospital y Paciente creadas

---
+++ src/HT8/Hospital.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HT8;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author esteb
+ */
+public class Hospital {
+    private ArrayList<Paciente> pacientes;
+    
+    public Hospital(){
+        this.pacientes = new ArrayList<Paciente>();
+    }
+}
